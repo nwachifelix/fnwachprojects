d41034b2dda2e74b26f0fd10460860870e96f6ba
nwachifelix <ogbonna54615@gmail.com>
2022-02-05 07:11:37 +0100

    Rolling three D6 dice and visuals.

---
+++ three_dice.py
@@ -0,0 +1,31 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+from die import Die
+
+# Create three D6 dice. 
+die_1 = Die(6)
+die_2 = Die(6)
+die_3 = Die(6)
+
+# Make some rolls, and store the results in a list. 
+results = [die_1.roll() + die_2.roll() + die_3.roll() for i in range(1000000)] # Activated List Comprehension. 
+# for roll_num in range(1000000):
+#     result = die_1.roll() + die_2.roll() + die_3.roll()
+#     results.append(result)
+
+# Analyze your results. 
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides + die_3.num_sides
+for value in range(3, max_result+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize your results. 
+x_values = list(range(3, max_result+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Results', 'dtick': 1}
+y_axis_config = {'title': 'Frequency of Results'}
+my_layout = Layout(title='Result of rolling three D6 1000000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
