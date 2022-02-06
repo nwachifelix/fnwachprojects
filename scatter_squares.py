4e3d9bdc982736041f7d8e4486d02e4111429c1c
nwachifelix <ogbonna54615@gmail.com>
2022-02-07 00:25:56 +0100

    Scatter quares.

---
+++ scatter_squares.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+
+# plt.style.use('seaborn')
+# fig, ax = plt.subplots()
+# ax.scatter(2, 4, s=200)
+
+# # Set chart title and label axes. 
+# ax.set_title('Square Numbers', fontsize=24)
+# ax.set_xlabel('Value', fontsize=14)
+# ax.set_ylabel('Square of Value', fontsize=14)
+
+# # Set size of tick labels. 
+# ax.tick_params(axis='both', which='major', labelsize=14)
+
+# plt.show() 
+
+# x_values = [1, 2, 3, 4, 5]
+# y_values = [1, 4, 9, 16, 25]
+
+# plt.style.use('seaborn')
+# fig, ax = plt.subplots()
+# ax.scatter(x_values, y_values, s=100)
+
+# Set chart title and label axes. 
+# ax.set_title('Square Numbers', fontsize=20)
+# ax.set_xlabel('Value', fontsize=14)
+# ax.set_ylabel('Square of Values', fontsize=14)
+
+
+
+# # Set chart title and label axes. 
+# plt.show()
+
+
+
+
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10) 
+
+# Set chart and label axes.
+ax.set_title('Square Numbers', fontsize=20)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Values', fontsize=14)
+
+#Set the range for each axis. 
+ax.axis([0, 1100, 0, 1100000])
+
+plt.savefig('squares_plot.png')
